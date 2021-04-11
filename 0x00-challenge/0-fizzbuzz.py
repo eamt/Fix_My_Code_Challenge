ce90b5fa07dc7e71d4eb526558d887021505c32a
eduard moreno <katananata500@gmail.com>
2021-04-11 17:13:55 +0000

    fizzbuzz 2

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -17,10 +17,10 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n+1):
-        if (i % 3) == 0 and (i % 5 is not 0):
-            tmp_result.append("Fizz")
-        if (i % 3 == 0) and (i % 5 == 0):
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        if (i % 3 == 0):
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
